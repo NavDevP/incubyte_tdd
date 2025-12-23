89970fe784d8192d146bfd8d6a53437a29405046
Naval Plahha <navalplahha@gmail.com>
2025-12-23 20:54:55 +0530

    Custom delimiter handled while included with numbers String

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ int add(String numbers) {
     return 0;
   }
 
+  var mainNumbersString = numbers;
   RegExp delimiter = RegExp(r'[\n,]');
 
-  return numbers
+  if (numbers.startsWith('//')) {
+    var parts = numbers.split('\n');
+    delimiter = RegExp(r'[' + parts[0].substring(2) + r']');
+    mainNumbersString = parts[1];
+  }
+
+  return mainNumbersString
       .split(delimiter)
       .map(int.parse)
       .reduce((a, b) => a + b);
--- test/main_test.dart
+++ test/main_test.dart
@@ -6,5 +6,8 @@ void main() {
     expect(add(""), 0);
     expect(add("1,2"), 3);
     expect(add("1,3\n2"), 6);
+    expect(add("//;\n1;2"), 3);
+    expect(add("//~\n1~2~50"), 53);
+    expect(add("//&\n1&2&2"), 5);
   });
 }
