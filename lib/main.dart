5ade37f6b6eaa1c84e45946bc050dafd3cbddb24
Naval Plahha <navalplahha@gmail.com>
2025-12-23 20:48:27 +0530

    Added new line seperator for handling Split

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ int add(String numbers) {
   if (numbers.isEmpty) {
     return 0;
   }
-  
+
+  RegExp delimiter = RegExp(r'[\n,]');
+
   return numbers
-      .split(',')
+      .split(delimiter)
       .map(int.parse)
       .reduce((a, b) => a + b);
 }
--- test/main_test.dart
+++ test/main_test.dart
@@ -5,5 +5,6 @@ void main() {
   test('add', () {
     expect(add(""), 0);
     expect(add("1,2"), 3);
+    expect(add("1,3\n2"), 6);
   });
 }
